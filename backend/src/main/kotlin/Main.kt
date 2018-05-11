9a0ca3cef4ecc1a540092a58fb88885b0081735d
Vladislav Khikhlov <vhihlov@rti-mints.ru>
2018-05-11 16:46:59 +0300

    Add Gson feature and Exposed basic example.

--- backend/src/main/kotlin/Main.kt
+++ backend/src/main/kotlin/Main.kt
@@ -1,11 +1,31 @@
 import io.ktor.application.*
+import io.ktor.features.ContentNegotiation
+import io.ktor.gson.GsonConverter
+import io.ktor.http.ContentType
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.jetbrains.exposed.sql.SchemaUtils.create
 
+data class City(val id: Int? = null, val name: String)
+
+object Cities : Table() {
+    val id = integer("id").autoIncrement().primaryKey()
+    var name = varchar("name", 50)
+}
 
 fun main(args: Array<String>) {
+    Database.connect("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", driver = "org.h2.Driver")
+    transaction {
+        logger.addLogger(StdOutSqlLogger)
+        create(Cities)
+        Cities.insert {
+            it[name] = "St. Petersburg"
+        } get Cities.id
+    }
     embeddedServer(
             Netty, watchPaths = listOf("backend"), port = 8080,
             module = Application::myModule
@@ -13,9 +33,16 @@ fun main(args: Array<String>) {
 }
 
 fun Application.myModule() {
+    install(ContentNegotiation) {
+        register(ContentType.Application.Json, GsonConverter())
+    }
     routing {
         get("/") {
-            call.respondText("Hello World!")
+            val city = transaction {
+                val row = Cities.selectAll().single()
+                City(row[Cities.id], row[Cities.name])
+            }
+            call.respond(city)
         }
     }
 }
